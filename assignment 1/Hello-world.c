279cfc575d8535e0fccddb5b014f5d65fbaf9673
PILLA NITISH KUMAR <63724986+nitish20899@users.noreply.github.com>
2021-01-29 12:10:14 +0530

    Update Hello-world.c

--- assignment 1/Hello-world.c
+++ assignment 1/Hello-world.c
@@ -1,7 +1,6 @@
 #include "contiki.h"
 #include <stdio.h>   
 #include <random.h> /* For random_rand() */
-#include <stdlib.h> /* For RAND_MAX */
 
 
 PROCESS(hello_world_process, "Hello world process");
@@ -13,15 +12,12 @@ PROCESS_THREAD(hello_world_process, ev, data)
   printf("Hello, world\n");
 
   printf("Generating 10 random numbers between 0 and 1\n\n");
-  float min_num = 0;
-  float max_num = 1.0;
   int count= 10;
   int i;
   float sum = 0;
   for (i = 0; i < count; i++) { 
-       double sc = (double)rand() / (double)RAND_MAX ;;
-       double ans = sc;
-       printf("%.8f\n", ans);
+       double sc = (double)rand() / (double)RAND_MAX ;
+       printf("%.8f\n", sc);
        sum = ans+sum;
   }
   printf("The sum of all the generated random numbers is %.8f\n", sum);
