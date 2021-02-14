e1fb7d405e5ea04fe227e3fb97bbb976e35597ee
PILLA NITISH KUMAR <63724986+nitish20899@users.noreply.github.com>
2021-02-14 18:46:36 +0530

    Add files via upload

---
+++ IOT/assignment 3/power.c
@@ -0,0 +1,44 @@
+#include "contiki.h"
+#include "net/rime.h"
+#include "random.h"
+
+#include "powertrace.h"
+
+#include "dev/button-sensor.h"
+
+#include "dev/leds.h"
+#include "dev/sht11-sensor.h"
+
+#include <stdio.h>
+/*---------------------------------------------------------------------------*/
+PROCESS(process1, "powertrace example");
+AUTOSTART_PROCESSES(&process1);
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(process1, ev, data)
+{
+  static struct etimer et;
+
+
+  PROCESS_BEGIN();
+
+
+  /* Start powertracing, once every second. */
+  powertrace_start(CLOCK_SECOND * 1);     
+  int number1 =10;
+  int number2 =20;
+  int sum;
+  sum = number1+number2;
+  printf("%d",sum);
+
+
+  while(1) {
+    /* Delay 2-4 seconds */
+    etimer_set(&et, CLOCK_SECOND * 4 + random_rand() % (CLOCK_SECOND * 4));
+
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
